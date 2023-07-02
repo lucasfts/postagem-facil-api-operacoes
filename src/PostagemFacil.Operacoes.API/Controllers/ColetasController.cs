d8ef0e3928850bbeb8b454e49d9fd87b924a2abf
Lucas Silva <silva.lucasfeitoza@gmail.com>
2023-07-02 16:57:43 -0300

    Renomeando coletas controller

--- src/PostagemFacil.Operacoes.API/Controllers/ColetasController.cs
+++ src/PostagemFacil.Operacoes.API/Controllers/ColetasController.cs
@@ -1,31 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PostagemFacil.Operacoes.API.Business;
 using PostagemFacil.Operacoes.API.Business.DTOs;
-using PostagemFacil.Operacoes.API.Data.Models;
 
 namespace PostagemFacil.Operacoes.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ColetaController : ControllerBase
+    public class ColetasController : ControllerBase
     {
         private readonly IColetaService _service;
 
-        public ColetaController(IColetaService service)
+        public ColetasController(IColetaService service)
         {
             _service = service;
         }
 
         [HttpGet]
-        public async Task<IActionResult> ObterSolicitacoes(int pagina = 1, int itensPorPagina = 10)
+        public async Task<IActionResult> ObterColetas(int pagina = 1, int itensPorPagina = 10)
         {
             var solicitacoes = await _service.ObterColetas(pagina, itensPorPagina);
             return Ok(solicitacoes);
         }
 
         [HttpPost]
-        public async Task<IActionResult> CriarSolicitacao(CriarColetaDTO dto)
+        public async Task<IActionResult> CriarColeta(CriarColetaDTO dto)
         {
             await _service.CriarColeta(dto);
             return Ok();
